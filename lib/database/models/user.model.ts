f7aafa621e6a7d22930ee9a543fa43786e83abb1
akash942 <67083278+akash942@users.noreply.github.com>
2025-05-07 23:55:20 +0530

    another small fix

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,7 +1,8 @@
 import { Schema, model, models } from "mongoose";
+import { ObjectId } from 'mongodb';
 
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
+  clerkId: { type: Schema.Types.ObjectId, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
